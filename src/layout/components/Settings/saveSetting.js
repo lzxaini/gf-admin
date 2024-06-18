9e21dbce1fe06b64db288ff4177e1e2e7a88ba27
17630921248 <1245634367@qq.com>
2024-06-18 14:44:40 +0800

    feature: 抽离主题保存方法

---
+++ src/layout/components/Settings/saveSetting.js
@@ -0,0 +1,24 @@
+/*
+ * @Author: 17630921248 1245634367@qq.com
+ * @Date: 2024-06-18 13:57:25
+ * @LastEditors: 17630921248 1245634367@qq.com
+ * @LastEditTime: 2024-06-18 14:15:31
+ * @FilePath: \RuoYi-Vue3\src\layout\components\Settings\saveSetting.js
+ * @Description: Fuck Bug
+ * 微信：My-World-40
+ */
+export default function saveSetting(modal, storeSettings) {
+  modal.loading("正在保存到本地，请稍候...");
+  let layoutSetting = {
+    "topNav": storeSettings.topNav,
+    "tagsView": storeSettings.tagsView,
+    "fixedHeader": storeSettings.fixedHeader,
+    "sidebarLogo": storeSettings.sidebarLogo,
+    "dynamicTitle": storeSettings.dynamicTitle,
+    "sideTheme": storeSettings.sideTheme,
+    "theme": storeSettings.theme,
+    "dark": storeSettings.dark
+  };
+  localStorage.setItem("layout-setting", JSON.stringify(layoutSetting));
+  setTimeout(modal.closeLoading(), 1000)
+}
