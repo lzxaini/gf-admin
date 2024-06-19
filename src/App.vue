97164ab07c351097c687bd24d2f99f85f6ae0c57
17630921248 <1245634367@qq.com>
2024-06-19 14:44:44 +0800

    feature: 入口文件注册国际化

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
-  <router-view />
+  <el-config-provider :locale="locale">
+    <router-view />
+  </el-config-provider>
 </template>
 
 <script setup>
 import useSettingsStore from '@/store/modules/settings'
+import { switchLanguages } from './language/index'
 import { handleThemeStyle } from '@/utils/theme'
-
+import { ElConfigProvider } from 'element-plus'
+const locale = switchLanguages(useSettingsStore().lang) // 获取对应的语言
 onMounted(() => {
   nextTick(() => {
     // 初始化主题样式
