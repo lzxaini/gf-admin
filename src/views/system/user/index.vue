0e0453f410e23c8f9b76861c6b4cd4e9b9eb04d2
17630921248 <1245634367@qq.com>
2025-07-28 11:08:30 +0800

    change: 去掉角色支持多选

--- src/views/system/user/index.vue
+++ src/views/system/user/index.vue
@@ -265,7 +265,7 @@
                </el-col>
                <el-col :span="12">
                   <el-form-item label="角色">
-                     <el-select v-model="form.roleIds" multiple placeholder="请选择">
+                     <el-select v-model="form.roleIds[0]" placeholder="请选择">
                         <el-option
                            v-for="item in roleOptions"
                            :key="item.roleId"
